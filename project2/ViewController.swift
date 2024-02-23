03f33072cf3dc772e38adde17c48f18e90e17585
enessozcann <e.ozcann99@gmail.com>
2024-02-23 14:51:20 +0300

    Initial Commit

---
+++ project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by EnesÖzcan on 23.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
