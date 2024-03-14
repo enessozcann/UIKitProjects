a07f1b9061eb05f1c5987f1386320e506a2b6ab2
enessozcann <e.ozcann99@gmail.com>
2024-03-14 22:53:29 +0300

    commit1

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MyCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Collection view için özel bir hücre sınıfı kaydetmek
+        self.collectionView!.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        
+        // Collection view'in düzenini ayarlamak
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        collectionView.setCollectionViewLayout(layout, animated: true)
     }
-
-
+    
+    // Collection view'deki hücre sayısını döndüren yöntem
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // Collection view'deki her hücreyi oluşturan yöntem
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! MyCollectionViewCell
+        cell.textLabel.text = "Item \(indexPath.item)"
+        return cell
+    }
+    
+    // Collection view'deki hücre boyutunu ayarlayan yöntem (UICollectionViewDelegateFlowLayout protokolünü uygulamalısınız)
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
 }
 
+// Özel bir hücre sınıfı oluşturmak
+class MyCollectionViewCell: UICollectionViewCell {
+    var textLabel: UILabel
+    
+    override init(frame: CGRect) {
+        self.textLabel = UILabel(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
+        super.init(frame: frame)
+        contentView.addSubview(textLabel)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
